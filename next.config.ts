065052843b1992a79e93ca224c5b07c5123acfed
afaque1996 <afaquemuzaddid@gmail.com>
2025-04-30 20:46:19 +0530

    Trigger first deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-    // Removing basePath since we're using a custom domain
-}
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Removing basePath since we're using a custom domain
+};
 
-export default nextConfig
+export default nextConfig;
