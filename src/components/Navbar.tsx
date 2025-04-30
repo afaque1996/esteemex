53cb0455b03d8e8634ca5dfc27c77e0b7bd867e6
afaque1996 <afaquemuzaddid@gmail.com>
2025-04-30 23:14:58 +0530

    Trigger redeploy: Added comment to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 'use client'
+// Updated mobile navbar with enhanced styling and animations
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
