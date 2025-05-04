0b31a8e7eaca50c1d0b990cade44e6c175a449f5
afaque1996 <afaquemuzaddid@gmail.com>
2025-05-04 22:20:01 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,7 @@ export default function Footer() {
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                     </svg>
-                                    F-203, Nerul Station Complex, Navi Mumbai – India
+                                    F-403, Thakur Castle, Taloja Phase-1, Raigarh, Navi Mumbai – India
                                 </p>
                                 <p className="flex items-center text-sm">
                                     <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
